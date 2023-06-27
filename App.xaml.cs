e79408b4d3e5a0fc22280ea7c0877f1105d013f3
stamenkovdmitriy <111305109+stamenkovdmitriy@users.noreply.github.com>
2023-06-27 10:50:59 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui_CSS;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Maui_CSS;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+    [Obsolete]
+    public MainPage()
+	{
+		InitializeComponent();
+
+        Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+        {
+            _timeLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+            return true;
+        });
+    }
+
+	
+}
+
